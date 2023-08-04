0815ec967c661a2ff9323c4fb70fd46f8aa05c30
igloude <igloude@gmail.com>
2023-08-04 12:43:55 -0500

    header styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from './icons/Logo.tsx';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <Logo />
+
+      <nav className='header__nav' role='navigation'>
+        <a href='#' className='link link__header'>Our work</a>
+        <a href='#' className='link link__header'>What we do</a>
+        <a href='#' className='link link__header'>Plans</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

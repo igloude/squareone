0b50198de7f23762ef88958a3285541b0f657836
igloude <igloude@gmail.com>
2023-08-04 15:02:42 -0500

    helpers

--- src/components/Capabilities.jsx
+++ src/components/Capabilities.jsx
@@ -4,7 +4,7 @@ const Card = (props) => {
   return (
     <div className="card stroke">
       <p><strong>{props.title}</strong></p>
-      <p className='t__subtext'>{props.children}</p>
+      <p className='subtext'>{props.children}</p>
     </div>
   );
 }
@@ -12,12 +12,15 @@ const Card = (props) => {
 const Capabilities = () => {
   return (
     <div className="capabilities" id="capabilities">
-      <Card title="Web Design">A branded site, splash page, event site, dashboard, the possibilities are endless.</Card>
-      <Card title="Email">Single emails or branded design templates for an all out e-campaign.</Card>
-      <Card title="Marketing Materials">Letterhead, business cards, resume, fliers, product packaging, documentation...</Card>
-      <Card title="Copywriting">Because sometimes words are hard.<br />We do good words.</Card>
-      <Card title="Design Systems">A suite of branded, bulletproof Figma components for product design.</Card>
-      <Card title="Frontend">We’ll do the HTML & CSS, you plug it in. It just works, right out of the box.</Card>
+      <h2 className='heading center'>What we do</h2>
+      <div className='cards'>
+        <Card title="Web Design">A branded site, splash page, event site, dashboard, the possibilities are endless.</Card>
+        <Card title="Email">Single emails or branded design templates for an all out e-campaign.</Card>
+        <Card title="Marketing Materials">Letterhead, business cards, resume, fliers, product packaging, documentation...</Card>
+        <Card title="Copywriting">Because sometimes words are hard.<br />We do good words.</Card>
+        <Card title="Design Systems">A suite of branded, bulletproof Figma components for product design.</Card>
+        <Card title="Frontend">We’ll do the HTML & CSS, you plug it in. It just works, right out of the box.</Card>
+      </div>
     </div>  
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
   return (
     <div className="hero">
-      <h1 className='t__hero'>Web design just got<br />crazy simple.</h1>
+      <h1 className='hero'>Web design just got<br />crazy simple.</h1>
       <p>Creative &amp; code as a service.</p>
     </div>
   );
